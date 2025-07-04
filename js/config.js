0344dd1bd50216bacf93febb8298090bb1faae3b
VINAY <168843336+Vinay00018@users.noreply.github.com>
2025-07-04 11:58:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_D7woYSK8I', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '53ie0v19iunv4gsckuems07eb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_CGntXXvKj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1k7bqajpil9jpudc0isd778ud', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
